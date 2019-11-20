e0332a59b2504dd59093924bf15d69069ae4e4c9
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-20 05:27:23 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,26 +6,31 @@ class Artist
 
   @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  def initialize
+     super
+     @songs = []
+   end
+
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +41,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
