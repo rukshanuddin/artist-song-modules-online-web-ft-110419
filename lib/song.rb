5b0167cc1751880bf5f82629ef2c3886b63e9dc5
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-20 05:12:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,18 +7,27 @@ class Song
   @@songs = []
 
   extend Memorable::ClassMethods
-
+  extend Findable::ClassMethods
+    
   def initialize
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
   end
+
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  # 
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
